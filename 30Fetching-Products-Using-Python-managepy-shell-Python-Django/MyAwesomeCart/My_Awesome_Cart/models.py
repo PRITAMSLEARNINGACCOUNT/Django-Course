6478230ac20af9b2a5a0b792a36637144b41352d
pritamstech <98208329+PRITAMSLEARNINGACCOUNT@users.noreply.github.com>
2023-10-26 11:11:36 -0900

    Add files via upload

---
+++ 30Fetching-Products-Using-Python-managepy-shell-Python-Django/MyAwesomeCart/My_Awesome_Cart/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Product(models.Model):
+    Product_ID = models.AutoField
+    Product_Name = models.CharField(max_length=50)
+    Product_Description = models.CharField(max_length=90)
+    Product_Price = models.IntegerField()
+    Product_Image = models.ImageField()
+
+    def __str__(self):
+        return self.Product_Name
